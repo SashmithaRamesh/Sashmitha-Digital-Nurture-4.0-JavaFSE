6777fbee35ec36380b040e26cdfe9863cafc76e2
SashmithaRamesh <sashmitharamesh16@gmail.com>
2025-07-27 22:05:48 +0530

    Delete Week 6 directory

--- Week 6/Additional/6.React.js – HOL/trainers_app/src/TrainersMock.js
+++
@@ -1,6 +0,0 @@
-import Trainer from './Trainer';
-const trainers = [
-  new Trainer(1, "Aathma", "aathma@gmail.com", "9999999991", "Java", "Spring, Hibernate"),
-  new Trainer(2, "Jojo", "jojo@gmail.com", "9999999992", "React", "React, Redux"),
-  new Trainer(3, "John", "john@gmail.com", "9999999993", "Python", "Django, Flask"), ];
-export default trainers;
